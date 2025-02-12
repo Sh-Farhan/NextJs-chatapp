f012cf742b3ae9af7fe1287e5a3fb85db347b058
Sh-Farhan <farhan.mh.shaikh@gmail.com>
2025-02-12 22:07:48 +0530

    integrated clerk auth and convex db

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+// https://relative-bengal-76.clerk.accounts.dev
+
+export default {
+    providers: [
+      {
+        domain: "https://relative-bengal-76.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
