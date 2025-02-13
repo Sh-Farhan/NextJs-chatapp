066d21388e91f547981a13ec2eef9cc58d2a3d49
Sh-Farhan <farhan.mh.shaikh@gmail.com>
2025-02-13 18:39:49 +0530

    integrated clerk webhook

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-// "use client"
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return (
-    <div>
-      <Button>Click</Button>
-    </div>
-  );
+  return(
+    <UserButton></UserButton>
+  )
 }
